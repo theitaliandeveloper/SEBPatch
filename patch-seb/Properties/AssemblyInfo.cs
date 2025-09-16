213ab539cf5434229a0cec8a345fb5a04d3954cc
Vichingo455 <vichingo455@gmail.com>
2025-09-16 16:51:14 +0200

    v1.7

--- patch-seb/Properties/AssemblyInfo.cs
+++ patch-seb/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Numero di build
 //      Revisione
 //
-[assembly: AssemblyVersion("1.6.0.1")]
-[assembly: AssemblyFileVersion("1.6.0.1")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
